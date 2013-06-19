0778085bd1453024e49f49496358dc4d204eceba
Takashi SASAKI <takashi316@gmail.com>
2013-06-19 20:57:20 +0900

    testCountUp passed

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Intervals():
         self.counter = [0] * N
 
     def countUp(self, i):
-        self.counter[i] += 1
+        self.counter[i - 1] += 1
 
     def sum(self, beginning, width):
         assert beginning >= 1
@@ -22,6 +22,18 @@ class Intervals():
             raise IndexError
         s = sum(self.series[beginning - 1:beginning - 1 + width])
         return s
+
+    def countUpForWidth(self, width):
+        for i in (range(1, len(self.series + 1))):
+            s = self.sum(i, width)
+            self.countUp(s)
+
+    def getCounter(self, i):
+        if i < 1: raise IndexError
+        return self.counter[i - 1]
+
+    def getLength(self):
+        return len(self.series)
 
 
 import unittest
@@ -49,6 +61,16 @@ class _TestCase(unittest.TestCase):
         self.assertEqual(self.intervals.sum(2, 3), 6)
         self.assertEqual(self.intervals.sum(3, 3), 6)
         self.assertRaises(IndexError, lambda: self.intervals.sum(4, 6))
+
+    def testCountUp(self):
+        self.assertEqual(self.intervals.counter[0], 0)
+        self.intervals.countUp(1)
+        self.assertEqual(self.intervals.counter[0], 1)
+        self.assertEqual(1, self.intervals.getCounter(1))
+        self.assertRaises(IndexError, lambda: self.intervals.getCounter(0))
+        self.assertEqual(self.N * 2 - 1, self.intervals.getLength())
+        self.assertEqual(0, self.intervals.getCounter(self.intervals.N))
+        self.assertRaises(IndexError, lambda: self.intervals.getCounter(self.intervals.N + 1))
 
 
 if __name__ == "__main__":
